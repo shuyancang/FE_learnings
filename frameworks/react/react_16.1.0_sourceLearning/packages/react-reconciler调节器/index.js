d531c03cd36bbd9b954681de32bbceebf35b1eb8
zwy414 <1551644698@qq.com>
2021-04-02 20:55:41 +0800

    fiber入口

--- frameworks/react/react_16.1.0_sourceLearning/packages/react-reconciler调节器/index.js
+++ frameworks/react/react_16.1.0_sourceLearning/packages/react-reconciler调节器/index.js
@@ -1,15 +1,7 @@
-/**
- * Copyright (c) 2013-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- * @flow
- */
+
 
 'use strict';
 
-// TODO: bundle Flow types with the package.
 export type {
   HostConfig,
   Deadline,
@@ -18,8 +10,6 @@ export type {
 
 var ReactFiberReconciler = require('./src/ReactFiberReconciler');
 
-// TODO: decide on the top-level export form.
-// This is hacky but makes it work with both Rollup and Jest.
 module.exports = ReactFiberReconciler.default
   ? ReactFiberReconciler.default
   : ReactFiberReconciler;
